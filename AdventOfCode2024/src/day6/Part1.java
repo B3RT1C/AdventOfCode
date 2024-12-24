aa14952a9f3e57ebf4cd6ab983ea3363ec1b9044
B3RT1C <151965724+B3RT1C@users.noreply.github.com>
2024-12-24 16:05:14 +0100

    Sorting information for easy use

--- AdventOfCode2024/src/day6/Part1.java
+++ AdventOfCode2024/src/day6/Part1.java
@@ -6,11 +6,20 @@ import java.util.Scanner;
 
 public class Part1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner("src/day6/input6.txt");
 
-        List<int[]> obstructuionsCords = new ArrayList<>();
+        List<int[]> obstructionsCords = new ArrayList<>();
         List<int[]> visitedCords = new ArrayList<>();
-        int[] guardCords = new int[2];
+
+        // 0 and 1 = cords, 2 and 3 = direction vector
+        int[] guardInfo = new int[4];
+
+
+
+
+    }
+
+    public static void gatherData(List<int[]> obstructionsCords, int[] guardInfo) {
+        Scanner in = new Scanner("src/day6/input6.txt");
 
         int y = 0;
         while (in.hasNextLine()) {
@@ -19,18 +28,41 @@ public class Part1 {
             int x = 0;
             for (char c : line.toCharArray()) {
                 if (c == '#') {
-                    obstructuionsCords.add(new int[]{y, x});
+                    obstructionsCords.add(new int[]{y, x});
 
-                } else if (c == '^' || c == '>' || c == 'v' || c == '<') {
-                    guardCords[0] = y;
-                    guardCords[1] = x;
+                } else if (c != '.') {
+                    guardInfo[0] = y;
+                    guardInfo[1] = x;
+
+                    switch(c) {
+                        case '^':
+                            guardInfo[2] = 0;
+                            guardInfo[3] = 0;
+                            break;
+                        case '>':
+                            guardInfo[2] = 1;
+                            guardInfo[3] = 0;
+                            break;
+                        case 'v':
+                            guardInfo[2] = 2;
+                            guardInfo[3] = 0;
+                            break;
+                        case '<':
+                            guardInfo[2] = 3;
+                            guardInfo[3] = 0;
+                            break;
+                    }
                 }
-
                 x++;
             }
-
-
             y++;
         }
+        in.close();
     }
+
+    public static void move(int[] guardInfo) {
+
+
+    }
+
 }
